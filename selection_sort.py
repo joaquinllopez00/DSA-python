64c670dc5aca2332efc0222ab02fc2d214c5f05e
joaquinllopez00 <joaquinllopezzz@gmail.com>
2021-11-17 12:36:32 -0500

    selection_sort algo

---
+++ selection_sort.py
@@ -0,0 +1,20 @@
+def findSmallest(arr):
+  smallest = arr[0] #stores smallest val
+  smallest_index = 0 # stores smallest index
+  for i in range(1, len(arr)):
+    if arr[i] < smallest:
+      smallest = arr[i]
+      smallest_index = i
+  return smallest_index
+
+
+def selectionSort(arr):
+  newArr = []
+  for i in range(len(arr)):
+    smallest = findSmallest(arr) # finds smallest
+    newArr.append(arr.pop(smallest)) #adds to new arr
+  print(newArr)
+  return newArr
+
+selectionSort([3,77,6,8,33,5]) # => [3,5,6,8,33,77]
+selectionSort(['hello', 'what', 'are', 'you', 'doing']) # => ['are', 'doing', 'hello', 'what', 'you']

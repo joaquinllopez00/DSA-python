e3c84bc8014e347582e48e21cb133d89c5e1609a
joaquinllopez00 <joaquinllopezzz@gmail.com>
2021-11-22 15:49:32 -0500

    factorial recursion func added

---
+++ recursion_factorial.py
@@ -0,0 +1,21 @@
+def fact(x):
+  if (x == 1):
+    return 1
+  else:
+    return x * fact(x-1)
+    
+
+"""
+Visualized example
+
+
+fact(3) => returns x * fact(3-1)
+fact(2) => returns x * fact(2-1)
+fact(1) => returns 1
+"""
+
+print(fact(3)) #=> 6
+print(fact(6)) #=> 720
+print(fact(998)) #=> a lot. 998 is recursion depth limit
+
+
